e5c929deadb87fe2029631ded7d93decf2b76db5
DiegoSB9 <diego_sb_95@hotmail.com>
2025-02-25 17:46:42 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -1,34 +1,52 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    full_name = Column(String(100))
+    bio = Column(Text)
+    profile_picture = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    image_url = Column(String(250), nullable=False)
+    caption = Column(Text)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    text = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship(User)
+    post = relationship(Post)
 
-## Draw from SQLAlchemy base
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship(User)
+    post = relationship(Post)
+
+## Generar el diagrama UML
 try:
     result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
     raise e
